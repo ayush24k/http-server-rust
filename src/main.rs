62ec3acc8794ca80301fd2289e7a0742c8de6c7e
ayush24k <ayushanshu69@gmail.com>
2024-07-03 19:08:33 +0530

    frontend changes

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use std::{
 use http_server_rust::ThreadPool;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
     let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
