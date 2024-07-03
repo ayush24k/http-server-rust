6b7e83ad6fa4165ecd172fbd20b13b8c8b475e8c
ayush24k <ayushanshu69@gmail.com>
2024-07-03 17:22:54 +0530

    added thread pool

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::{
-    fmt::{format, Result}, fs, io::{prelude, BufRead, BufReader, Write}, net::{TcpListener, TcpStream}
+    fmt::{format, Result},
+    fs,
+    io::{prelude, BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream}, thread, time::Duration,
 };
 
 fn main() {
@@ -14,17 +17,21 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-            .lines()
-            .map(|result| result.unwrap())
-            .take_while(|line| !line.is_empty())
-            .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("index.html").unwrap();
+    
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),  
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent-Lenght: {length}\r\n\r\n{contents}"); // HTTP-Version Status-Code Reason-Phrase CRLF headers(empty) CRLFm essage-body(empty)
-
-    stream.write_all(response.as_bytes()).unwrap();
-} 
+        let response = format!("{status_line}\r\nContent-Lenght: {length}\r\n\r\n{contents}"); // HTTP-Version Status-Code Reason-Phrase CRLF headers(empty) CRLFm essage-body(empty)
+        stream.write_all(response.as_bytes()).unwrap();
+}
